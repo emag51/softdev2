870461df49b0621f1c98610bc53143e9188cb79d
emag51 <61967786+emag51@users.noreply.github.com>
2022-12-19 11:16:33 +0700

    error.htnl

--- workshop4/index.js
+++ workshop4/index.js
@@ -1,26 +1,25 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('สวัสดีวันนี้วันพุธ')
-})
+// app.get('/', (req, res) => {
+//     res.send('สวัสดีวันนี้วันพุธ')
+// })
 
-app.get('/home', function(req, res){
-    res.sendFile(__dirname + '/home.html')
-})
+// app.get('/home', function(req, res){
+//     res.sendFile(__dirname + '/home.html')
+// })
 
-app.get('/about', function(req, res){
-    res.sendFile(__dirname + '/about.html')
-})
+// app.get('/about', function(req, res){
+//     res.sendFile(__dirname + '/about.html')
+// })
 
-app.get('/product', function(req, res){
-    res.sendFile(__dirname + '/product.html')
-})
+// app.get('/product', function(req, res){
+//     res.sendFile(__dirname + '/product.html')
+// })
 
-app.get('*', (req, res) => {
-    res.send('Error: 404 Page Not Found')
+app.get('*', function(req, res) {
+    res.sendFile(__dirname + '/error.html')
 })
-
 app.listen(3000, () => {
     console.log("Start Server at port [3000")
 })

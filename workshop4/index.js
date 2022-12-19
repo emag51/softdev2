5f7918c2148df718e1f3332147c205805b622d70
emag51 <61967786+emag51@users.noreply.github.com>
2022-12-19 10:36:09 +0700

    firstcommit

---
+++ workshop4/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('สวัสดีวันนี้วันพุธ')
+})
+
+app.get('/home', function(req, res){
+    res.sendFile(__dirname + '/home.html')
+})
+
+app.get('/about', function(req, res){
+    res.sendFile(__dirname + '/about.html')
+})
+
+app.get('/product', function(req, res){
+    res.sendFile(__dirname + '/product.html')
+})
+
+app.get('*', (req, res) => {
+    res.send('Error: 404 Page Not Found')
+})
+
+app.listen(3000, () => {
+    console.log("Start Server at port [3000")
+})

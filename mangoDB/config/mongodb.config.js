2d0c47ede156a42aed76f84502a57ce8140dc63a
emag51 <61967786+emag51@users.noreply.github.com>
2023-01-11 11:16:40 +0700

    server

--- mangoDB/config/mongodb.config.js
+++ mangoDB/config/mongodb.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://127.0.0.1:27017/customercrud'
+    // url: 'mongodb://127.0.0.1:27017/customercrud'
+    url: 'mongodb+srv://emag51:12345@clsoutheastasia.axy2g5c.mongodb.net/?retryWrites=true&w=majority'
 }
